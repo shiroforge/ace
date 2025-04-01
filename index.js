145642a321266566d1c898dde5365b9841a69d11
fugo8015276 <sf.engineer20231201@gmail.com>
2025-04-01 20:49:39 +0900

    fechAPIでXMLHttpRequestを受け取る

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 function fetchUserInfo(userId) {
-    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
-        .then(response => {
-            console.log(response.status);
-            // エラーレスポンスが返されたことを検知する
-            if (!response.ok) {
-                console.error("エラーレスポンス", response);
-            } else {
-                return response.json().then(userInfo => {
-                    console.log(userInfo);
-                });
-            }
-        }).catch(error => {
-            console.error(error);
-        });
+    // リクエストを作成する
+    const request = new XMLHttpRequest();
+    request.open("GET", `https://api.github.com/users/${encodeURIComponent(userId)}`);
+    request.addEventListener("load", () => {
+        // リクエストが成功したかを判定する
+        // Fetch APIのresponse.okと同等の意味
+        if (request.status >= 200 && request.status < 300) {
+            // レスポンス文字列をJSONオブジェクトにパースする
+            const userInfo = JSON.parse(request.responseText);
+            console.log(userInfo);
+        } else {
+            console.error("エラーレスポンス", request.statusText);
+        }
+    });
+    request.addEventListener("error", () => {
+        console.error("ネットワークエラー");
+    });
+    // リクエストを送信する
+    request.send();
 }
